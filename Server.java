898890f5568ec4290903b6b3dfbc4bf942bdbe04
eplaza4 <143279350+eplaza4@users.noreply.github.com>
2023-11-30 00:52:40 -0500

    Add files via upload

--- Server.java
+++ Server.java
@@ -11,64 +11,64 @@ import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class Server extends Application {
-  @Override // Override the start method in the Application class
-  public void start(Stage primaryStage) {
-    // Text area for displaying contents
-    TextArea ta = new TextArea();
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        // Text area for displaying contents
+        TextArea ta = new TextArea();
 
-    // Create a scene and place it in the stage
-    Scene scene = new Scene(new ScrollPane(ta), 450, 200);
-    primaryStage.setTitle("Server"); // Set the stage title
-    primaryStage.setScene(scene); // Place the scene in the stage
-    primaryStage.show(); // Display the stage
-    
-    new Thread( () -> {
-      try {
-        // Create a server socket
-        ServerSocket serverSocket = new ServerSocket(8000);
-        Platform.runLater(() ->
-                ta.appendText("Server started at " + new Date() + '\n'));
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(new ScrollPane(ta), 450, 200);
+        primaryStage.setTitle("Server"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
 
-        // Listen for a connection request
-        Socket socket = serverSocket.accept();
+        new Thread( () -> {
+            try {
+                // Create a server socket
+                ServerSocket serverSocket = new ServerSocket(8000);
+                Platform.runLater(() ->
+                        ta.appendText("Server started at " + new Date() + '\n'));
 
-        // Create data input and output streams
-        DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-        DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-  
-        while (true) {
-          // Receive number from the client
-          int number = inputFromClient.readInt();
-  
-          // Check if the number is prime
-          boolean prime = isPrime(number);
-  
-          // Send the response back to the client
-          outputToClient.writeUTF(prime ? "Yes" : "No");
-  
-          Platform.runLater(() -> {
-            ta.appendText("Number received from client: " + number + '\n');
-            ta.appendText("Is prime: " + (prime ? "Yes" : "No") + '\n');
-          });
-        }
-      }
-      catch(IOException ex) {
-        ex.printStackTrace();
-      }
-    }).start();
-  }
+                // Listen for a connection request
+                Socket socket = serverSocket.accept();
 
-// Method to check if a number is prime
-  private static boolean isPrime ( int number){
-    if (number <= 1) return false;
-    for (int i = 2; i <= Math.sqrt(number); i++) {
-      if (number % i == 0) return false;
+                // Create data input and output streams
+                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+
+                while (true) {
+                    // Receive number from the client
+                    int number = inputFromClient.readInt();
+
+                    // Check if the number is prime
+                    boolean prime = isPrime(number);
+
+                    // Send the response back to the client
+                    outputToClient.writeUTF(prime ? "Yes" : "No");
+
+                    Platform.runLater(() -> {
+                        ta.appendText("Number received from client: " + number + '\n');
+                        ta.appendText("Is prime: " + (prime ? "Yes" : "No") + '\n');
+                    });
+                }
+            }
+            catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }).start();
     }
-    return true;
-  }
 
-  // The main method is only needed for the IDE with limited JavaFX support
-  public static void main(String[] args) {
-    launch(args);
-  }
+    // Method to check if a number is prime
+    private static boolean isPrime ( int number){
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    // The main method is only needed for the IDE with limited JavaFX support
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
